2697e4df301bb0d1be3f2bfccf9d80b80772c431
JacoboCarta <jacobocartagenag@gmail.com>
2026-05-21 22:48:36 -0500

    Update conection.py

--- conection.py
+++ conection.py
@@ -1,16 +1,17 @@
 import oracledb
 import streamlit as st
+import os
 
 oracledb.init_oracle_client(lib_dir=r"C:\oracle\instantclient_23_26")
 
 @st.cache_resource
 def get_connection():
     conn = oracledb.connect(
-        user="",
-        password="",
-        dsn="",
-        config_dir=r"",
-        wallet_location=r"",
+        user=os.getenv("ORACLE_USER", "G08_E01"),
+        password=os.getenv("ORACLE_PASSWORD", ""),
+        dsn=os.getenv("ORACLE_DSN", "adbg08_tp"),
+        config_dir=os.getenv("ORACLE_WALLET", ""),
+        wallet_location=os.getenv("ORACLE_WALLET", ""),
         wallet_password=""
     )
     return conn

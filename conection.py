2628b25d741c0a2154927be601daa7cde56ac74b
JacoboCarta <jacobocartagenag@gmail.com>
2026-05-21 22:46:19 -0500

    Update conection.py

--- conection.py
+++ conection.py
@@ -6,11 +6,11 @@ oracledb.init_oracle_client(lib_dir=r"C:\oracle\instantclient_23_26")
 @st.cache_resource
 def get_connection():
     conn = oracledb.connect(
-        user="G08_E01",
-        password="Bi!2026-08-01",
-        dsn="adbg08_tp",
-        config_dir=r"C:\oracle\wallet_g08",
-        wallet_location=r"C:\oracle\wallet_g08",
+        user="",
+        password="",
+        dsn="",
+        config_dir=r"",
+        wallet_location=r"",
         wallet_password=""
     )
     return conn
